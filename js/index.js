f79b84789728e6234acdae7978da44c19cd11ec5
Luis Mora <luisemorav@hotmail.com>
2022-09-27 19:46:09 -0500

    modal activo

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let sliderSectionLast = sliderSection[sliderSection.length - 1];
 const btnLeft = document.querySelector("#btn-left");
 const btnRight = document.querySelector("#btn-right");
 
-slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+slider.insertAdjacentElement("afterbegin", sliderSectionLast);
 
 function Next() {
   let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
@@ -13,22 +13,21 @@ function Next() {
   slider.style.transition = "all 0.5s";
   setTimeout(function () {
     slider.style.transition = "none";
-    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.insertAdjacentElement("beforeend", sliderSectionFirst);
     slider.style.marginLeft = "-100%";
   }, 500);
 }
 
 function Prev() {
-let sliderSection = document.querySelectorAll(".slider__section");
-let sliderSectionLast = sliderSection[sliderSection.length - 1];
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length - 1];
   slider.style.marginLeft = "0";
   slider.style.transition = "all 0.5s";
   setTimeout(function () {
     slider.style.transition = "none";
-    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.insertAdjacentElement("afterbegin", sliderSectionLast);
     slider.style.marginLeft = "-100%";
   }, 500);
-  
 }
 
 btnRight.addEventListener("click", function () {
@@ -39,6 +38,13 @@ btnLeft.addEventListener("click", function () {
   Prev();
 });
 
-setInterval(function(){
-    Next();
+setInterval(function () {
+  Next();
 }, 4000);
+
+function CloseModal() {
+  document.querySelector(".modal").style.display = "none";
+}
+
+document.querySelector("#closeModal").addEventListener("click", CloseModal);
+
